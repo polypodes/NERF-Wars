89948af7b80ba824057c74c5bba6c337b458ec18
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-08-29 23:28:04 +0200

    testing eventEmitting

--- test/Timer.js
+++ test/Timer.js
@@ -1,16 +1,39 @@
-var Timer   = require('../lib/Timer');
-var assert  = require("assert");
+var Timer   = require('../lib/Timer')
+, assert  = require("assert")
+, sinon = require('sinon')
+, EventEmitter = require('events').EventEmitter;
+
 
 describe('Timer', function(){
 
     describe('.start()', function(){
-        it('should return true when the timer starts', function(){
+        it('should return a callback when the timer starts', function(done){
             var timer = new Timer();
-            assert.ok(timer.start());
+            timer.start(function(err) {
+                if (err) throw err;
+                done();
+            });
+            timer.stop();
+        })
+    })
+
+    describe('.start()', function(){
+        it('should emit a "start" event with a timestamp value when the timer starts', function(){
+            var spy1 = sinon.spy();
+            var spy2 = sinon.spy();
+            var timer = new Timer();
+            timer.on("start", spy1);
+            timer.on("this does not exist", spy2);
+            timer.start();
+            assert.ok(spy1.called);
+            assert.ok(!spy2.called);
             timer.stop();
             delete timer;
         })
     })
+
+
+
 
     describe('.start()', function(){
         it('should throw an error when the timer is already started', function(){
@@ -22,13 +45,33 @@ describe('Timer', function(){
     })
 
     describe('.stop()', function(){
-        it('should return true when the timer is started', function(){
+        it('should return a callback when the timer stops', function(done){
             var timer = new Timer();
             timer.start();
-            assert.ok(timer.stop());
+            timer.stop(function(err) {
+                if (err) throw err;
+                done();
+            });
             delete timer;
         })
     })
+
+    describe('.stop()', function(){
+        it('should emit a "stop" event with a timestamp value when the timer stops', function(){
+            var spy1 = sinon.spy();
+            var spy2 = sinon.spy();
+            var timer = new Timer();
+            timer.start();
+            timer.on("stop", spy1);
+            timer.on("this does not exist", spy2);
+            timer.stop();
+            assert.ok(spy1.called);
+            assert.ok(!spy2.called);
+            delete timer, spy1, spy2
+        })
+    })
+
+
 
     describe('.stop()', function(){
         it('should throw an error when the timer is not started', function(){
