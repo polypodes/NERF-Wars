098e9377b8f30d9e62733fe11ed5ba38ecb7ac7b
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-08-29 21:20:43 +0200

    better testing

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ NERF-Wars
 =========
 
 NERF wars sessions manager: random start, ending coutdown, music, helicopters
+
+
+## Tests
+
+
+```bash
+npm install -g mocha
+mocha
+```
--- test/Timer.js
+++ test/Timer.js
@@ -1,26 +1,42 @@
-var Timer   = require('../lib/Timer').Timer;
+var Timer   = require('../lib/Timer');
 var assert  = require("assert");
 
 describe('Timer', function(){
 
-    describe('#start()', function(){
+    describe('.start()', function(){
         it('should return true when the timer starts', function(){
             var timer = new Timer();
-            console.log(timer);
             assert.ok(timer.start());
             timer.stop();
             delete timer;
         })
     })
 
-    describe('#start()', function(){
-        it('should throw an error when the timer is not already started', function(){
-            var timer1 = new Timer();
-            var timer2 = new Timer();
-            assert.throws(timer2.start());
-            delete timer1;
-            delete timer2;
+    describe('.start()', function(){
+        it('should throw an error when the timer is already started', function(){
+            var timer = new Timer();
+            timer.start();
+            assert.throws(timer.start);
+            delete timer;
         })
     })
+
+    describe('.stop()', function(){
+        it('should return true when the timer is started', function(){
+            var timer = new Timer();
+            timer.start();
+            assert.ok(timer.stop());
+            delete timer;
+        })
+    })
+
+    describe('.stop()', function(){
+        it('should throw an error when the timer is not started', function(){
+            var timer = new Timer();
+            assert.throws(timer.stop);
+            delete timer;
+        })
+    })
+
 })
 
