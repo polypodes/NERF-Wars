87c43f93511f167611de4ff981ba98130bfb59da
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-09-01 10:42:10 +0200

    improving code coverage adding new tests

--- test/Timer.js
+++ test/Timer.js
@@ -34,6 +34,7 @@ describe('Timer', function(){
             timer.start();
             setTimeout(function () {
                 timer.stop();
+                delete timer;
                 done();
             }, 500);
             clock.tick(500);
@@ -48,8 +49,21 @@ describe('Timer', function(){
                 done();
             });
             timer.stop();
+            delete timer;
         });
     });
+
+    describe('.reset()', function(){
+        it('should run a callback', function(done){
+            var timer = new Timer();
+            timer.reset(function(err) {
+                if (err) throw err;
+                delete timer;
+                done();
+            });
+        });
+    });
+
 
     describe('.start()', function(){
         it('should emit a "start" event with a timestamp value when the timer starts', function(){
@@ -69,7 +83,7 @@ describe('Timer', function(){
         it('should throw an error when the timer is already started', function(){
             var timer = new Timer();
             timer.start();
-            assert.throws(timer.start);
+            assert.throws(timer.start(), Error);
             delete timer;
         });
     });
@@ -116,6 +130,25 @@ describe('Timer', function(){
         });
     });
 
+    describe('.getFormatedTime()', function(){
+        it('should return a non-empty string', function(){
+            var callback = sinon.spy();
+            var timer = new Timer();
+            timer.on("stop", callback);
+            timer.start();
+            var timedOut = false;
+            setTimeout(function () {
+                timedOut = true;
+            }, 500);
+            clock.tick(510);
+            var ellapsed = timer.getFormatedTime();
+            timer.stop();
+            var duration = timer.getDuration();
+            assert.ok(ellapsed.length > 0);
+            delete timer;
+        })
+    })
+
     describe('.getDuration()', function(){
         it('should return a millisecond value, > 0', function(){
             var callback = sinon.spy();
@@ -142,9 +175,23 @@ describe('Timer', function(){
                 timer.addEvent('testEvent #'+i, eventTime);
             }
             assert.ok(timer.getEventList().length > 0 && 11 > timer.getEventList().length);
+            timer.start();
+            timer.stop();
             delete timer;
         })
     })
+
+     describe('.removeEvent(s)', function() {
+        it('should remove an identified event', function() {
+            var timer = new Timer();
+            timer.addEvent('testEventA', 1);
+            timer.addEvent('testEventB', 2);
+            timer.removeEvent('testEventA');
+            assert.ok(timer.getEventList().pop().identifier === 'testEventB');
+            delete timer;
+
+        });
+    });
 
     describe('.addEvent(s)', function() {
         it('should rise an event after s seconds', function(done) {
@@ -154,6 +201,8 @@ describe('Timer', function(){
             timer.addEvent('testEvent', 1);
             var subscriptionToTestEvent = timer.subscribe('testEvent', function(e){
                 callback();
+                timer.stop();
+                delete timer;
                 done();
             });
             timer.start();
@@ -165,7 +214,6 @@ describe('Timer', function(){
             assert.ok(!callback.calledOnce && !timedOut);
             clock.tick(1000);
             assert.ok(callback.calledOnce && timedOut);
-            timer.stop();
         });
     });
 })
